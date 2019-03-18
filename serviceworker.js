af85c10212b1cd98c1e386239cbf75236d50c72f
Your Name <you@example.com>
2019-03-18 10:38:44 +0700

    push notif

--- serviceworker.js
+++ serviceworker.js
@@ -77,3 +77,16 @@ else {
   //     )
   //   );
   // });
+  self.addEventListener('notificationclick', function(e){
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = e.action;
+
+    if(action==='close'){
+      notification.close();
+
+    }else{
+      clients.openWindow('http://google.com');
+      notification.close();
+    }
+  })
